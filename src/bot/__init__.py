ea4a3a5bb00dc7cb6c098424bc6de2ddae9b4bc7
Lukramancer <57191063+Lukramancer@users.noreply.github.com>
2024-06-15 12:57:55 +0500

    Added command to upload

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -8,7 +8,7 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 from sqlalchemy.orm import Session
 
-from src.database import Upload, register_user_if_not_exists
+from src.database import Upload, register_user_if_not_exists, User
 from src.s3 import FileStorage
 from .messages import get_formatted_message
 
@@ -24,6 +24,35 @@ async def main(token: str, file_storage: FileStorage, database_session: Session,
         register_user_if_not_exists(database_session, user_id)
 
         await message.reply(get_formatted_message("hello", message))
+
+    @dispatcher.message(F.text.startswith("/get_upload"))
+    async def command_get_upload(message: Message):
+        user_id = message.from_user.id
+        register_user_if_not_exists(database_session, user_id)
+
+        user_role = database_session.query(User).get(user_id).role
+
+        if user_role != "admin":
+            await message.reply(get_formatted_message("get_upload_command/permission_denied", message))
+            return
+
+        try:
+            upload_id = int(message.text.split(' ')[1])
+        except IndexError:
+            await message.reply(get_formatted_message("get_upload_command/no_upload_id", message))
+            return
+        except ValueError:
+            await message.reply(get_formatted_message("get_upload_command/upload_id_parse_error", message))
+            return
+
+        upload: Upload | None = database_session.query(Upload).get(upload_id)
+        if upload is None:
+            await message.reply(get_formatted_message("get_upload_command/not_found_upload", message, {
+                "upload_id": str(upload_id)}))
+        else:
+            await message.reply(get_formatted_message("get_upload_command/result", message, {
+                "input_image_url": file_storage.get_file_url(upload.input_image_key),
+                "output_image_url": file_storage.get_file_url(upload.output_image_key)}))
 
     @dispatcher.message(F.photo)
     async def on_photo_handler(message: Message) -> None:
